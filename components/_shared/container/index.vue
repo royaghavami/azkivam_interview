daadc1840903208d854d24a67dedf674c84531a5
Roya Ghavami <R.Ghavami@Jabama.com>
2023-12-11 20:15:33 +0330

    feat: accordion for categories and divider

---
+++ components/_shared/container/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="accordion"><slot /></div>
+</template>
+
+<script setup>
+import { computed, provide, ref } from 'vue'
+
+// Holds the id of the currently expanded panel
+const expanded = ref(null)
+
+// Use `provide` to communicate with the child panels
+provide('accordion-register', () => {
+  const id = Symbol()
+
+  return {
+    expanded: computed(() => expanded.value === id),
+
+    toggle() {
+      expanded.value = expanded.value === id ? null : id
+    },
+
+    unregister() {
+      if (expanded.value === id) {
+        expanded.value = null
+      }
+    },
+  }
+})
+</script>
+
+<style scoped>
+.accordion {
+  border: 1px solid #ccc;
+  display: flex;
+  flex-direction: column;
+  height: 300px;
+  width: 240px;
+}
+</style>
--- components/plp/index.vue
+++ components/plp/index.vue
@@ -1,13 +1,38 @@
 <template>
-  <section style="display: flex; justify-content: center; margin-top: 10rem">
+  <Container class="container">
+    <h2 class="container-title">
+      فیلتر ها
+      <br />
+      دسته بندی
+    </h2>
+    <Accordion
+      v-for="(category, index) in categories"
+      :key="index"
+      :title="category.name"
+      class="accordion"
+    >
+      <div
+        v-for="(child, i) in category.children"
+        :key="i"
+        class="accordion-wrapper"
+      >
+        <NuxtLink
+          :to="`/products/${child.id}/${child.slug}`"
+          class="accordion-wrapper__child"
+          >{{ child.name }}</NuxtLink
+        >
+      </div>
+    </Accordion>
+    <span class="container-divider" />
+  </Container>
+  <section>
     <div class="product-list">
-      <!--    <NuxtLink :to="`products/1/oenwlg`">khvgfjufv</NuxtLink>-->
-      <div class="e" v-for="item in items" :key="item.id">
+      <div v-for="item in items" :key="item.id" class="e">
         <Card
           :id="item.id"
           :name="item.name"
           :image-url="item.imageUrl"
-          :price="item.price"
+          :price="item.minPrice"
         />
       </div>
     </div>
@@ -15,11 +40,40 @@
 </template>
 <script lang="ts" setup>
 import Card from '@/components/_shared/card/index.vue'
+import Container from '@/components/_shared/container/index.vue'
+import Accordion from '@/components/_shared/accordion/index.vue'
 defineProps<{
   items: any
+  categories: any
 }>()
 </script>
 <style lang="scss" scoped>
+.container {
+  padding: 0 17px 0 0;
+  border-radius: 10px;
+  height: auto;
+  &-title {
+    font-weight: bold;
+    font-size: 16px;
+    line-height: 3;
+  }
+  &-divider {
+    height: 1px;
+    margin: 0 0 16px 33px;
+    border-left: 12rem solid lightgray;
+  }
+}
+.accordion {
+  color: black;
+  margin-bottom: 18px;
+  &-wrapper {
+    margin: 12px 24px;
+    &__child {
+      color: gray;
+      text-decoration: none;
+    }
+  }
+}
 .product-list {
   display: grid;
   grid-gap: 0px 0px;
